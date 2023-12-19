95c65ed02d08ba55615c63da024449dd84cf0165
KarnPrasai <karn_prasai@cmu.ac.th>
2023-12-19 15:56:47 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    int N_data = 0;
+    double sum, sum_sq;
+    string scoreline;
+    ifstream source("score.txt");
+    while(getline(source,scoreline))
+    {
+        sum += stod(scoreline);
+        sum_sq += pow(stod(scoreline),2);
+        N_data++;
+    }
+    cout << "Number of data = "<< N_data <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< sum / N_data <<"\n";
+    cout << "Standard deviation = " << sqrt((sum_sq/N_data)-pow(sum/N_data,2));
+}
